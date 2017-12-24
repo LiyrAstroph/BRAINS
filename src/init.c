9d902113533eaf48811c8bdbeb235940880b425c
Li Yan-Rong <liyanrong@lxslc607.ihep.ac.cn>
2017-12-24 23:50:28 +0800

    fix a bug in setting upper limit of fflow for model5. fix improper initial values in sim.c for model5.

--- src/init.c
+++ src/init.c
@@ -536,7 +536,7 @@ void set_blr_range_model5()
   blr_range_model[i++][1] = 1.0;
   //fflow
   blr_range_model[i][0] = 0.0;
-  blr_range_model[i++][1] = 0.1;
+  blr_range_model[i++][1] = 1.0;
   //sigr_circ
   blr_range_model[i][0] = log(0.001);
   blr_range_model[i++][1] = log(0.1);
