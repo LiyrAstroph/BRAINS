0062d5f81a9d901ed63ef5c09c28a55176ea8d5e
Yan-Rong Li (Dell Laptop) <liyanrong@mail.ihep.ac.cn>
2020-04-27 18:41:05 +0800

    remove duplicated lines in transfun.c.

--- src/transfun.c
+++ src/transfun.c
@@ -195,52 +195,9 @@ void calculate_line2d_from_blrmodel(const void *pm, const double *Tl, const doub
  */
 void transfun_1d_cal_cloud(const void *pm, int flag_save)
 {
-  int i, idt;
-  double tau_min, tau_max, dTransTau;
-  double Anorm, dis;
-
   /* generate cloud sample and calculate the corresponding time lags and weights */
   gen_cloud_sample(pm, 1, flag_save);
-
-  tau_min = clouds_tau[0];
-  tau_max = clouds_tau[0];
-  for(i=0; i<parset.n_cloud_per_task; i++)
-  {
-    if(tau_min > clouds_tau[i])
-      tau_min = clouds_tau[i];
-    if(tau_max < clouds_tau[i])
-      tau_max = clouds_tau[i];
-  }
-
-  dTransTau = (tau_max - tau_min)/(parset.n_tau - 1);
-  for(i=0; i<parset.n_tau; i++)
-  {
-    TransTau[i] = tau_min + dTransTau * i;
-    Trans1D[i] = 0.0;
-  }
-
-  for(i=0; i<parset.n_cloud_per_task; i++)
-  {
-    dis = clouds_tau[i];
-    idt = (dis - tau_min)/dTransTau;
-    //Trans1D[idt] += pow(1.0/r, 2.0*(1 + gam)) * weight;
-    Trans1D[idt] += clouds_weight[i];
-  }
-
-  /* normalize transfer function */
-  Anorm = 0.0;
-  for(i=0;i<parset.n_tau;i++)
-  {
-    Anorm += Trans1D[i];
-  }
-  Anorm *= dTransTau;
-
-  Anorm += EPS;
-
-  for(i=0; i<parset.n_tau; i++)
-  {
-    Trans1D[i] /= Anorm;
-  }
+  transfun_1d_cal_with_sample();
   return;
 }
 
@@ -301,67 +258,9 @@ void transfun_1d_cal_with_sample()
  */
 void transfun_2d_cal_cloud(const void *pm, double *transv, double *trans2d, int n_vel, int flag_save)
 {
-  int i, j, idt, idV;
-  double tau_min, tau_max, dTransTau;
-  double Anorm, dis, V, dV;
-
   /* generate cloud sample and calculate the corresponding time lags, LOS velocity, and weights */
   gen_cloud_sample(pm, 2, flag_save);
-
-  tau_min = clouds_tau[0];
-  tau_max = clouds_tau[0];
-  for(i=0; i<parset.n_cloud_per_task; i++)
-  {
-    if(tau_min > clouds_tau[i])
-      tau_min = clouds_tau[i];
-    if(tau_max < clouds_tau[i])
-      tau_max = clouds_tau[i];
-  }
-
-  dTransTau = (tau_max - tau_min)/(parset.n_tau - 1);
-  for(i=0; i<parset.n_tau; i++)
-  {
-    TransTau[i] = tau_min + dTransTau * i;
-  }
-
-  dV =(transv[1] - transv[0]); /* velocity grid width */
-  for(i=0; i<parset.n_tau; i++)
-    for(j=0;j<n_vel;j++)
-      trans2d[i*n_vel+j]=0.0;   /* cleanup of transfer function */
-
-  for(i=0; i<parset.n_cloud_per_task; i++)
-  {
-    dis = clouds_tau[i];
-    idt = (dis - tau_min)/dTransTau;
-
-    for(j=0; j<parset.n_vel_per_cloud; j++)
-    {
-      V = clouds_vel[i*parset.n_vel_per_cloud + j];
-      if(V<transv[0] || V >= transv[n_vel-1]+dV)
-        continue;
-      idV = (V - transv[0])/dV;
-      //trans2d[idt*n_vel + idV] += pow(1.0/r, 2.0*(1 + gam)) * weight;
-      trans2d[idt*n_vel + idV] += clouds_weight[i];
-    }
-  }
-
-  /* normalize transfer function */
-  Anorm = 0.0;
-  for(i=0; i<parset.n_tau; i++)
-    for(j=0; j<n_vel; j++)
-    {
-      Anorm += trans2d[i*n_vel+j];
-    }
-  Anorm *= (dV * dTransTau);
-
-  Anorm += EPS;
-  for(i=0; i<parset.n_tau; i++)
-  {
-    for(j=0; j<n_vel; j++)
-    {
-      trans2d[i*n_vel+j] /= Anorm;
-    }
-  }
+  transfun_2d_cal_with_sample(transv, trans2d, n_vel);
   return;
 }
 
